e290b4d0634993fa3b951f44d735026c44f90944
Pablo Valverde <pblvalverde@gmail.com>
2025-05-30 01:33:30 -0500

    Eliminado Sidebar globalmente: menú lateral de módulos ya no aparece en ninguna página.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
-import Sidebar from './Sidebar';
 
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <div className="flex flex-1">
-        <Sidebar />
         <main className="flex-1 p-4 md:p-6">
           <Outlet />
         </main>
@@ -16,4 +14,4 @@ const Layout = () => {
   );
 };
 
-export default Layout; 
+export default Layout;
